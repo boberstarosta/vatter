b4c9588c1ebe4103516ab8681bcdca35ad66e5dc
boberstarosta <boberstarosta@gmail.com>
2018-02-22 23:47:59 +0100

    Added style to buttons

--- vatter/gui/views.py
+++ vatter/gui/views.py
@@ -25,14 +25,14 @@ class DetailView(tk.Toplevel):
             data_frame.grid_rowconfigure(i, weight=1)
             tk.Label(data_frame, text=field.caption + ':', anchor='e')\
                 .grid(column=0, row=i, sticky='ew', **settings.GRID_STYLE)
-            tk.Label(data_frame, text=getattr(self.obj, field.column_name), anchor='w')\
+            tk.Label(data_frame, text=getattr(self.obj, field.column_name), anchor='w', width=80)\
                 .grid(column=1, row=i, sticky='ew', **settings.GRID_STYLE)
 
         buttons_frame = tk.Frame(self)
         buttons_frame.pack(fill='x', expand=True)
-        tk.Button(buttons_frame, text='Edytuj').pack(side='left', **settings.PACK_STYLE)
-        tk.Button(buttons_frame, text='Usuń').pack(side='left', **settings.PACK_STYLE)
-        tk.Button(buttons_frame, text='Zamknij').pack(side='left', **settings.PACK_STYLE)
+        tk.Button(buttons_frame, text='Edytuj', **settings.BUTTON_STYLE).pack(side='left', **settings.PACK_STYLE)
+        tk.Button(buttons_frame, text='Usuń', **settings.BUTTON_STYLE).pack(side='left', **settings.PACK_STYLE)
+        tk.Button(buttons_frame, text='Zamknij', **settings.BUTTON_STYLE).pack(side='left', **settings.PACK_STYLE)
 
 
 class CustomerDetailView(DetailView):
